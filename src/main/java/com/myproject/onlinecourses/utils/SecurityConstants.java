3c7949c5d1a82f097cec918c9a5c346f3f49f27c
ToanNPT <thanhtoannguyen08102001@gmail.com>
2022-10-06 13:59:23 +0700

    update token 30days

--- src/main/java/com/myproject/onlinecourses/utils/SecurityConstants.java
+++ src/main/java/com/myproject/onlinecourses/utils/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.myproject.onlinecourses.utils;
 
 public class SecurityConstants {
     public static final String SECRET = "toandeptrai";
-    public static final long EXPIRATION_TIME = 60 * 60 * 24 * 1000; // 1 day
+    public static final long EXPIRATION_TIME = 60 * 60 * 24 * 30 * 1000; // 1 day
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/api/v1/login";
