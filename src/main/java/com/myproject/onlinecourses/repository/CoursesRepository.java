654d286d64bd239ad187b720ac193182931fbc9b
ToanNPT <thanhtoannguyen08102001@gmail.com>
2022-11-18 11:13:44 +0700

    fix get by id and all course

--- src/main/java/com/myproject/onlinecourses/repository/CoursesRepository.java
+++ src/main/java/com/myproject/onlinecourses/repository/CoursesRepository.java
@@ -1,11 +1,14 @@
 package com.myproject.onlinecourses.repository;
 
 import com.myproject.onlinecourses.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CoursesRepository extends JpaRepository<Course, String>, JpaSpecificationExecutor<Course> {
 
@@ -29,4 +32,15 @@ public interface CoursesRepository extends JpaRepository<Course, String>, JpaSpe
             "order by c.num_students desc " +
             "limit :limit", nativeQuery = true)
     List<Course> findByTopNumStudents(int limit);
+
+    @Query("select c " +
+            "from Course as c " +
+            "where c.id = :id and c.isActive = true")
+    Optional<Course> findById(String id);
+
+    @Query("select c " +
+            "from Course as c " +
+            "where c.isActive = true " +
+            "order by c.createDate desc")
+    Page<Course> findAll(Pageable pageable);
 }
