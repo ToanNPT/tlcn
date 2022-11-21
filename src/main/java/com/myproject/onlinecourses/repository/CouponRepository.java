377ceb7943676150b56e2bb63745d99f6d0cb76a
ToanNPT <thanhtoannguyen08102001@gmail.com>
2022-11-21 14:08:39 +0700

    fix api get all coupons v2

--- src/main/java/com/myproject/onlinecourses/repository/CouponRepository.java
+++ src/main/java/com/myproject/onlinecourses/repository/CouponRepository.java
@@ -1,6 +1,8 @@
 package com.myproject.onlinecourses.repository;
 
 import com.myproject.onlinecourses.entity.Coupon;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +11,9 @@ import java.util.Optional;
 public interface CouponRepository extends JpaRepository<Coupon, String> {
     @Query("select c from Coupon as c where c.code = :code and c.isActive = true")
     Optional<Coupon> findByCode(String code);
+
+    @Query("select c " +
+            "from Coupon as c " +
+            "where c.isActive = true")
+    Page<Coupon> findAll(Pageable pageable);
 }
